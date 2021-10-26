d2d30182f13ab56a3c0601db4c55c86f172b922a
unloccode <unloccode@gmail.com>
2021-10-26 17:38:15 +0300

    minor change 11.8

--- src/components/Starter.js
+++ src/components/Starter.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Homer from './Homer';
 
+//the date field will acts as a marker
+//marker for keeping track of the user task
+//and the marker, which essentially is a date, will be used to filter, sort and do basic analysis
 const DB = [
     {tskHead: 'code', tksBody: 'one of the finest code', tskDate: '16:44'},
 ];
