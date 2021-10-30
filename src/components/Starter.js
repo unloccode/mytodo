ba8f47a91194df0c3a44794b97e4a75c41f3247c
unloccode <unloccode@gmail.com>
2021-10-30 19:05:28 +0300

    change log 12.7

--- src/components/Starter.js
+++ src/components/Starter.js
@@ -4,12 +4,12 @@ import Homer from './Homer';
 //the date field will acts as a marker
 //marker for keeping track of the user task
 //and the marker, which essentially is a date, will be used to filter, sort and do basic analysis
-const DB = [
-    {tskHead: 'code', tksBody: 'one of the finest code', tskDate: '16:44', timeStamp: '28102021'},
-    {tskHead: 'glass animals', tksBody: 'Heat waves', tskDate: '15:21', timeStamp: '29102021'},
-    {tskHead: 'slim shady', tksBody: 'Not afraid', tskDate: '12:07', timeStamp: '27102021'},
-];
-//const DB = [];
+//const DB = [
+//    {tskHead: 'code', tksBody: 'one of the finest code', tskDate: '16:44', timeStamp: '28102021'},
+//    {tskHead: 'glass animals', tksBody: 'Heat waves', tskDate: '15:21', timeStamp: '29102021'},
+//    {tskHead: 'slim shady', tksBody: 'Not afraid', tskDate: '12:07', timeStamp: '31102021'},
+//];
+const DB = [];
 //now the task bundle contains a marker, and that marker is a unique id
 //the marker is assigned a timestamp, this is the unique identifier
 
