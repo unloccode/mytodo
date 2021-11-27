d3bc64e353ef9fffa89ff177a39a0581f0fa1bcc
unloccode <unloccode@gmail.com>
2021-11-27 20:39:25 +0300

    change log 16.0

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //const API_URL = "http://localhost:8080/api/auth/";
 //const API_URL = "http://192.168.1.12:8080/api/auth/";
-const API_URL = "https://keeptaskserver.herokuapp.com/api/auth";
+const API_URL = "https://keeptaskserver.herokuapp.com/api/auth/";
 
 class AuthService{
     login(email, password){
